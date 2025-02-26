1a6e67f5d586872c41d2052e4e7f418b269ec469
SpandanMukherjee <spandan.mukherjee428@gmail.com>
2025-02-26 17:15:27 +0530

    Finalized Search Button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function search(event, ele) {
-
-    if (event.key === 'Enter') {
+    
+    if (event.key === 'Enter' || event.type === "click") {
         let query = encodeURIComponent(ele.value.trim());
         let engine = document.getElementById('search-engine').value;
         
@@ -9,3 +9,14 @@ function search(event, ele) {
         }
     }
 }
+
+let searchButton = document.getElementById("search-btn");
+let inputField = document.getElementById("input");
+
+inputField.addEventListener("keydown", function(event) {
+    search(event, inputField);
+});
+
+searchButton.addEventListener("click", function(event) {
+    search({type : "click"}, inputField);
+});
